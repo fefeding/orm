4705732556b61ff314df2f6f9ead36f1f908932a
jiamao <haofefe@163.com>
2019-04-11 09:29:00 +0800

    types

--- helper/db.js
+++ helper/db.js
@@ -1,80 +1,28 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference path="../typings/index.d.ts" />
 /**
  * 提供DB操作基础库
  * 支持分页等功能
  */
-var DBHelper = /** @class */ (function () {
-    function DBHelper() {
-    }
+class DBHelper {
     /**
      * 使用SQL执行
      * 例如 pars = {"sql": "select * from table where id=? and title like ?", params: [1, '%abc%']}
      * @param {IDBSqlParam} pars 执行sql参数
      * @returns {IDBResult}
      */
-    DBHelper.query = function (pars) {
-        return __awaiter(this, void 0, void 0, function () {
-            var result, _a;
-            return __generator(this, function (_b) {
-                switch (_b.label) {
-                    case 0:
-                        result = {
-                            data: null
-                        };
-                        _a = result;
-                        return [4 /*yield*/, pars.db.query(pars.sql, pars.params)];
-                    case 1:
-                        _a.data = _b.sent();
-                        return [2 /*return*/, result];
-                }
-            });
-        });
-    };
-    DBHelper.update = function (pars) {
-        return __awaiter(this, void 0, void 0, function () {
-            return __generator(this, function (_a) {
-                pars.sql += '1';
-                return [2 /*return*/, 1];
-            });
-        });
-    };
-    return DBHelper;
-}());
-exports["default"] = DBHelper;
+    static async query(pars) {
+        let result = {
+            data: null
+        };
+        result.data = await pars.db.query(pars.sql, pars.params);
+        return result;
+    }
+    static async update(pars) {
+        pars.sql += '1';
+        return 1;
+    }
+}
+exports.default = DBHelper;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZGIuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJkYi50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLDhDQUE4QztBQUM5Qzs7O0dBR0c7QUFDSCxNQUFNLFFBQVE7SUFDVjs7Ozs7T0FLRztJQUNILE1BQU0sQ0FBQyxLQUFLLENBQUMsS0FBSyxDQUFDLElBQWlCO1FBQ2hDLElBQUksTUFBTSxHQUFHO1lBQ1QsSUFBSSxFQUFFLElBQUk7U0FDYixDQUFDO1FBQ0YsTUFBTSxDQUFDLElBQUksR0FBRyxNQUFNLElBQUksQ0FBQyxFQUFFLENBQUMsS0FBSyxDQUFDLElBQUksQ0FBQyxHQUFHLEVBQUUsSUFBSSxDQUFDLE1BQU0sQ0FBQyxDQUFDO1FBQ3pELE9BQU8sTUFBTSxDQUFDO0lBQ2xCLENBQUM7SUFFRCxNQUFNLENBQUMsS0FBSyxDQUFDLE1BQU0sQ0FBQyxJQUFpQjtRQUNqQyxJQUFJLENBQUMsR0FBRyxJQUFJLEdBQUcsQ0FBQztRQUNoQixPQUFPLENBQUMsQ0FBQztJQUNiLENBQUM7Q0FDSjtBQUVELGtCQUFlLFFBQVEsQ0FBQyJ9
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
-exports.__esModule = true;
-var base_1 = require("./model/base");
-var db_1 = require("./helper/db");
-exports.BaseModel = base_1["default"];
-exports.DBHelper = db_1["default"];
+Object.defineProperty(exports, "__esModule", { value: true });
+const base_1 = require("./model/base");
+const db_1 = require("./helper/db");
+exports.BaseModel = base_1.default;
+exports.DBHelper = db_1.default;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUNBLHVDQUFnQztBQUNoQyxvQ0FBNkI7QUFFaEIsUUFBQSxTQUFTLEdBQUcsY0FBSSxDQUFDO0FBQ2pCLFFBQUEsUUFBUSxHQUFHLFlBQUUsQ0FBQyJ9
